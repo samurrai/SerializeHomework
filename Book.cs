dc4a21372b5c5af315a0e5d630ebed3e239a4735
samurrai <samurrrai.r@gmail.com>
2019-03-22 12:55:23 +0600

    Домашка

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeHomework
+{
+    [Serializable]
+    [Book(PagesAmount = 250)]
+    public class Book
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book()
+            {
+                Name = "Harry Potter",
+                Price = 4500,
+                Author = "J.K.Rowling", 
+                Year = 2011
+            };
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream writer = new FileStream("data.txt", FileMode.Create))
+            {
+                formatter.Serialize(writer, book);
+            }
+            Book newBook = new Book();
+            using (FileStream writer = new FileStream("data.txt", FileMode.Open))
+            {
+                newBook = formatter.Deserialize(writer) as Book;
+            }
+            Console.WriteLine(newBook.Name);
+            var type = typeof(Book);
+            var val = type.CustomAttributes;
+            foreach (var element in val)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
